f3cacc75c43ff3fb6595eb00e6aa715d342d46df
Dylan Fitzgerald <dylan@dylanfitzgerald.net>
2025-06-21 11:20:49 -0600

    fix: Correct PromEx plugin configuration
    - Use PhoenixLiveView instead of LiveView
    - Add proper Ecto configuration with repo
    - Fix dashboard reference

--- lib/rsolv/prom_ex.ex
+++ lib/rsolv/prom_ex.ex
@@ -10,8 +10,8 @@ defmodule RSOLV.PromEx do
       Plugins.Application,
       Plugins.Beam,
       {Plugins.Phoenix, router: RSOLVWeb.Router, endpoint: RSOLVWeb.Endpoint},
-      Plugins.Ecto,
-      Plugins.LiveView,
+      {Plugins.Ecto, otp_app: :rsolv_api, repos: [RsolvApi.Repo]},
+      Plugins.PhoenixLiveView,
       
       # Custom plugin for rate limiter metrics
       RSOLV.PromEx.RateLimiterPlugin
@@ -34,7 +34,7 @@ defmodule RSOLV.PromEx do
       {:prom_ex, "beam.json"},
       {:prom_ex, "phoenix.json"},
       {:prom_ex, "ecto.json"},
-      {:prom_ex, "live_view.json"},
+      {:prom_ex, "phoenix_live_view.json"},
       
       # Custom dashboards
       {:otp_app, "grafana_dashboards/rate_limiter.json"}
